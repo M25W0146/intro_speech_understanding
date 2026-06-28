61a15d6edf00738353dbedf589dff217cf6360e9
M25W0146 <st142533@m2.kcg.edu>
2026-06-29 06:32:59 +0900

    Add files via upload

--- lec11/homework11.py
+++ lec11/homework11.py
@@ -14,11 +14,11 @@ def transcribe_wavefile(filename, language):
     text (str) - the recognized speech
     '''
     #raise RuntimeError("FAIL!!  You need to change this function so it works!")
-    r = speech_recognition.Recognizer()
+    r = sr.Recognizer()
 
-    with speech_recognition.AudioFile(filename) as source:
+    with sr.AudioFile(filename) as source:
         audio = r.record(source)
         text = r.recognize_google(audio, language=language)
-
+    
     return text
         
